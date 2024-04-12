c0206f8dc739d085a8d1b8b2e43cffe0dc34b0c7
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-04-12 13:46:25 +0900

    Fix lint error

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
+	"log"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/hello", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":8080"))
+	log.Println("hello world")
 }

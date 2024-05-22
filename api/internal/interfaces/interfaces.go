f73da2f5548b3a1095cd54fc66ca613fe41a7708
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-05-22 11:53:18 +0900

    Fix interface

--- api/internal/interfaces/interfaces.go
+++ api/internal/interfaces/interfaces.go
@@ -2,25 +2,10 @@ package interfaces
 
 import "github.com/Fumiya-Tahara/dev-navi/internal/entity"
 
-// Add method for CRUD
-//Service is the interface for web APIs, while repository is the interface for database operations
-
-type UserService interface {
-}
-
-type ProjectService interface {
-}
-
-type MilestoneService interface {
-}
-
-// Adding methods grouped under 'issues'
-type UserRepository interface{}
-
 type ProjectRepository interface {
 	Create(project *entity.Project) error
 	FindById(id uint) (*entity.Project, error)
-	FindByUserId(userId uint) ([]*entity.Project, error)
+	FindByUserId(userID uint) ([]*entity.Project, error)
 	Update(project *entity.Project) error
 	Delete(project *entity.Project, id uint) error
 }

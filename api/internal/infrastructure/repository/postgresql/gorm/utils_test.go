e576e2a025163b0dfbc22feb24318ec3111f12ce
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-07-06 00:35:46 +0900

    Add: crud test util

---
+++ api/internal/infrastructure/repository/postgresql/gorm/utils_test.go
@@ -0,0 +1,24 @@
+package gorm
+
+import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"gorm.io/driver/postgres"
+
+	"gorm.io/gorm"
+)
+
+func SetUpDBMock() (*gorm.DB, sqlmock.Sqlmock, error) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	gdb, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: db,
+	}), &gorm.Config{})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return gdb, mock, nil
+}

63ad52d3b547cf53a2c35b9da596ccc514ed8e69
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-07-05 11:30:01 +0900

    Fix: table structure

--- api/migrations/000004_create_milestones_table.up.sql
+++ api/migrations/000004_create_milestones_table.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE "milestones" (
     "id" bigserial PRIMARY KEY,
     "project_id" bigint NOT NULL,
     "title" varchar NOT NULL,
+    "status" smallint NOT NULL,
     "deadline" timestamp,
     CONSTRAINT fk_milestones_projects FOREIGN KEY (project_id) REFERENCES projects(id)
 )

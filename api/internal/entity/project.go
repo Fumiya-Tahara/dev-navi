41889aadfedd5eb6d362590be8267deb7c0abcc0
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-07-05 11:31:28 +0900

    Fix: entities field

--- api/internal/entity/project.go
+++ api/internal/entity/project.go
@@ -10,6 +10,7 @@ type Project struct {
 	ID        uint           `json:"id"`
 	Title     string         `json:"title"`
 	Deadline  time.Time      `json:"deadline"`
+	Status    uint           `json:"status"`
 	Memo      string         `json:"memo"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`

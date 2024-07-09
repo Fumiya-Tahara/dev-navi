dd5d7f01f8babaed938c5d5841cc74e0e8f9ae71
Fumiya Tahara <84490421+Fumiya-Tahara@users.noreply.github.com>
2024-07-09 20:58:12 +0900

    Revert "Fix: split domain entities and database models"

--- api/internal/entity/user.go
+++ api/internal/entity/user.go
@@ -1,6 +1,16 @@
 package entity
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type User struct {
-	Name     string `json:"name"`
-	Password string `json:"password"`
+	ID        uint           `json:"id"`
+	Name      string         `json:"name"`
+	Password  string         `json:"password"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
 }

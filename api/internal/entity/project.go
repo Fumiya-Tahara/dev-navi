62c3e145a19c19fd3b1d5f2d9a1a78bfe61c84d9
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-07-09 20:21:07 +0900

    Fix: split domain entities and database models

--- api/internal/entity/project.go
+++ api/internal/entity/project.go
@@ -2,17 +2,11 @@ package entity
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Project struct {
-	ID        uint           `json:"id"`
-	Title     string         `json:"title"`
-	Deadline  time.Time      `json:"deadline"`
-	Status    uint           `json:"status"`
-	Memo      string         `json:"memo"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+	Title    string    `json:"title"`
+	Deadline time.Time `json:"deadline"`
+	Status   uint      `json:"status"`
+	Memo     string    `json:"memo"`
 }
--- api/internal/entity/user.go
+++ api/internal/entity/user.go
@@ -1,16 +1,6 @@
 package entity
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
-
 type User struct {
-	ID        uint           `json:"id"`
-	Name      string         `json:"name"`
-	Password  string         `json:"password"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
 }

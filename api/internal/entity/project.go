9bd0f72db110f22da6cebc5d8f4f0b33d33ac810
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-07-10 09:28:48 +0900

    Fix: entity

--- api/internal/entity/project.go
+++ api/internal/entity/project.go
@@ -2,17 +2,12 @@ package entity
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Project struct {
-	ID        uint           `json:"id"`
-	Title     string         `json:"title"`
-	Deadline  time.Time      `json:"deadline"`
-	Status    uint           `json:"status"`
-	Memo      string         `json:"memo"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+	ID       uint      `db:"id"`
+	Title    string    `db:"title"`
+	Deadline time.Time `db:"deadline"`
+	Status   uint      `db:"status"`
+	Memo     string    `db:"memo"`
 }
--- api/internal/entity/user.go
+++ api/internal/entity/user.go
@@ -1,16 +1,7 @@
 package entity
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
-
 type User struct {
-	ID        uint           `json:"id"`
-	Name      string         `json:"name"`
-	Password  string         `json:"password"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+	ID       uint   `db:"id"`
+	Name     string `db:"name"`
+	Password string `db:"password"`
 }
